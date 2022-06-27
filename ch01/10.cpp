962794ed659329470c8364cc7c5a43a41e5dec64
beenchangseo <beenchangseo@gmail.com>
2022-06-27 15:23:08 +0900

    BaekJoon:2559 / atoi() / string to int 와 int to string

---
+++ ch01/10.cpp
@@ -0,0 +1,17 @@
+/**
+ * BaekJoon 1620번 /
+ * string to int 와 int to string 비교
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+
+    return 0;
+}
+
